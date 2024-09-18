9bb8130691262ac680d65f45ae83295bb7ce0b1c
remco van der Velde <remco.pc@outlook.com>
2024-09-18 12:21:24 +0200

    generate command

--- src/Service/Security.php
+++ src/Service/Security.php
@@ -39,7 +39,8 @@ class Security extends Main
         ){
             $permissions[] = $name_permission . ':' . $function_permission . '.' . 'parse';
         }
-        ddd(get_class_methods($role));
+        d($role);
+        d(get_class_methods($role));
         if(method_exists($role, 'getPermissions')){
             $role_permissions = (array) $role->getPermissions();
             foreach ($role_permissions as $nr => $role_permission){
