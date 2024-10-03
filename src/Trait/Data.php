82dc08528d76beba584ef5ef66312a38e138cc0b
remco van der Velde <remco.pc@outlook.com>
2024-10-03 18:35:05 +0200

    mb_strtolower / mb_strtoupper replace

--- src/Trait/Data.php
+++ src/Trait/Data.php
@@ -399,7 +399,6 @@ trait Data {
     {
         $properties = [];
         echo 'Leave "name" empty if finished.' . PHP_EOL;
-        fwrite(STDOUT, 'Leave "name" empty if finished.' . PHP_EOL);
         while(true){
             $name = Cli::read('input', 'Enter the "name" of the property: ');
             if(empty($name)){
