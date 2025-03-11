cf5170e2e96db18d2d7a139583882939164c3259
remco van der Velde <remco.pc@outlook.com>
2025-03-11 17:15:01 +0100

    init

--- src/Trait/Relation.php
+++ src/Trait/Relation.php
@@ -183,6 +183,7 @@ trait Relation {
                                         $this->role_system(),
                                         $one_many
                                     );
+                                    d($response);
                                     if(
                                         !empty($response) &&
                                         array_key_exists('list', $response)
