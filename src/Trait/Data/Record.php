9422109c3520202fc899a480c7706c8ecbc74e99
remco van der Velde <remco.pc@outlook.com>
2025-03-21 09:39:48 +0100

    sqlite

--- src/Trait/Data/Record.php
+++ src/Trait/Data/Record.php
@@ -48,7 +48,6 @@ trait Record {
             ];
         }
         $response = $this->list($name, $role, $options);
-        ddd($response);
         unset($name);
         unset($options);
         unset($role);
