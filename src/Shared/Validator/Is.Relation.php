e0de4fc7f0130f0921eace6ce56ad887d4b4167e
remco van der Velde <remco.pc@outlook.com>
2025-03-22 22:44:17 +0100

    sqlite

--- src/Shared/Validator/Is.Relation.php
+++ src/Shared/Validator/Is.Relation.php
@@ -36,25 +36,32 @@ function validate_is_relation(App $object, object $record, mixed $array=null, mi
                 return true;
             }
             if(strlen($value) !== 36){
+                d('1');
                 return false;
             }
             $explode = explode('-', $value);
             if(count($explode) !== 5){
+                d('1');
                 return false;
             }
             if(strlen($explode[0]) !== 8){
+                d('1');
                 return false;
             }
             if(strlen($explode[1]) !== 4){
+                d('1');
                 return false;
             }
             if(strlen($explode[2]) !== 4){
+                d('1');
                 return false;
             }
             if(strlen($explode[3]) !== 4){
+                d('1');
                 return false;
             }
             if(strlen($explode[4]) !== 12){
+                d('1');
                 return false;
             }
         }
