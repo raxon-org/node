42887d97bf733a61c14d885ee262ced72c8c9d39
remco van der Velde <remco.pc@outlook.com>
2025-03-23 18:49:22 +0100

    sqlite

--- src/Service/Security.php
+++ src/Service/Security.php
@@ -47,6 +47,7 @@ class Security extends Main
         if(is_array($role_permissions)){
             foreach($role_permissions as $permission){
                 $permission = new Data($permission);
+                d($permission);
                 if($permission->get('name') === $name_permission . ':' .$function_permission){
                     $is_permission = true;
                 }
