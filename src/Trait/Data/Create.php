89257a3e25ee8ee52ab146420becc59028fe4a22
remco van der Velde <remco.pc@outlook.com>
2025-05-02 15:50:43 +0200

    implementing new parser

--- src/Trait/Data/Create.php
+++ src/Trait/Data/Create.php
@@ -191,7 +191,7 @@ trait Create {
                             array_key_exists('event', $options) &&
                             $options['event'] === true
                         ){
-                            ddd($options);
+                            ddd($record);
                             Event::trigger($object, 'raxon.org.node.create', [
                                 'class' => $name,
                                 'node' => $record,
