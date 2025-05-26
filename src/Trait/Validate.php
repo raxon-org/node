6ad78f4f1f78d3626a1c5271b9bff7e006992446
remco van der Velde <remco.pc@outlook.com>
2025-05-26 14:41:37 +0200

    init

--- src/Trait/Validate.php
+++ src/Trait/Validate.php
@@ -31,6 +31,7 @@ trait Validate {
                 throw new Exception('No validation found for ' . $type . ' in ' . $url . '.');
             }
         }
+        d($function);
         return false;
     }
 }
