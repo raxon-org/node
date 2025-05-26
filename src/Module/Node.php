e13e280010688f3ba58c94dd4aa2bddd0a700192
remco van der Velde <remco.pc@outlook.com>
2025-05-26 15:25:18 +0200

    init

--- src/Module/Node.php
+++ src/Module/Node.php
@@ -19,7 +19,7 @@ class Node extends Main {
      */
     public function __construct(App $object){
         $this->object($object);
-        $autoload = $object->data();
+        $autoload = $object->data(App::AUTOLOAD_RAXON);
         ddd($autoload);
         $this->storage(new Storage());
     }
