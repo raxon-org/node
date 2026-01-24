4826352f28f2f5a1d3019b0b69b27f6634dd7baa
Remco van der Velde <remco@universeorange.com>
2026-01-24 17:00:01 +0100

    pp

--- src/Trait/Data/Import.php
+++ src/Trait/Data/Import.php
@@ -604,6 +604,7 @@ trait Import {
             }
         }
         if(!empty($patch_many)){
+            trace();
             d($patch_many);
             $response = $this->patch_many($name, $role, $patch_many, [
                 'import' => true,
