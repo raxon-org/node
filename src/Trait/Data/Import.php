62f1a8559192efe9445783b3a0bfb784c117bbf3
Remco van der Velde <remco@universeorange.com>
2026-01-24 14:37:26 +0100

    make raxon/doctrine default for sqlite user database

--- src/Trait/Data/Import.php
+++ src/Trait/Data/Import.php
@@ -559,6 +559,7 @@ trait Import {
         $put = 0;
         $patch = 0;
         $create = 0;
+        ddd($options);
         if(!empty($create_many)) {
             $response = $this->create_many($name, $role, $create_many, [
                 'import' => true,
