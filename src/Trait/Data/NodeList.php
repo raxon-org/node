89ee2804e1ac1f5f1c31031f62c7821de2c25e8b
Remco van der Velde <remco@universeorange.com>
2026-03-10 17:25:03 +0100

    init

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -95,6 +95,7 @@ trait NodeList {
                 is_array($options['where'])
             ){
                 $where = $this->list_where($options);
+                breakpoint($where);
             }
             $options['where'] = $where;
         }
