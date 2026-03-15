9ed10426642c9f5eb344a053398a56c777c08956
Remco van der Velde <remco@universeorange.com>
2026-03-15 21:10:27 +0100

    issue create

--- src/Trait/Filter.php
+++ src/Trait/Filter.php
@@ -15,7 +15,10 @@ trait Filter {
 
         $list = [];
         $list[] = $record;
+        d($filter);
+        d($list);
         $list = Module::list($list)->where($filter);
+        breakpoint($list);
         if(!empty($list)){
             return $record;
         }
