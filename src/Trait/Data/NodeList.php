06d499cd727b6f1ea577e11ec03d7ff8fe364d11
Remco van der Velde <remco@universeorange.com>
2026-03-21 17:06:20 +0100

    init

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -1661,6 +1661,7 @@ trait NodeList {
 
     private function nodelist_output_filter(App $object, $list, $options=[]): mixed
     {
+        d($options);
         if(!array_key_exists('output', $options)){
             return $list;
         }
