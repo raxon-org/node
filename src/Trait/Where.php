000b00d6743b4eba72babc6361a43dadf59987e9
Remco van der Velde <remco@universeorange.com>
2026-03-22 22:30:51 +0100

    init

--- src/Trait/Where.php
+++ src/Trait/Where.php
@@ -101,6 +101,7 @@ trait Where {
         $next = null;
         $skip = 0;
         $tree = [];
+        ddd($list);
         foreach($list as $nr => $record){
             $record = $this->where_operator_add($record);
             if($skip > 0){
