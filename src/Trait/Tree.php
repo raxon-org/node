3e1fa52d2175f2a6ee0ecd53edf9d1a0690eae8f
Remco van der Velde <remco@universeorange.com>
2026-03-22 22:14:46 +0100

    init

--- src/Trait/Tree.php
+++ src/Trait/Tree.php
@@ -88,7 +88,7 @@ trait Tree {
      */
     private function tree_record_attribute($record=[]): mixed
     {
-        if(!array_key_exists('type', $record)){
+        if(is_array($record) && !array_key_exists('type', $record)){
             trace();
             ddd($record);
         }
