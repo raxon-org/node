c925515511c373fcbbbc327a02c17800c0d5829b
Remco van der Velde <remco@universeorange.com>
2026-03-22 21:58:09 +0100

    init

--- src/Trait/Tree.php
+++ src/Trait/Tree.php
@@ -89,81 +89,13 @@ trait Tree {
      */
     private function tree_record_attribute($record=[]): mixed
     {
-        $attribute = '';
-        ddd($record);
-        if(!array_key_exists('collection', $record)){
-            switch($record['type']){
-                case Token::TYPE_QUOTE_DOUBLE_STRING:
-                    if(strpos($record['value'], '{') === false){
-                        return substr($record['value'], 1, -1);
-                    }
-                    //parse string...
-                    $object = $this->object();
-                    $storage = $this->storage();
-                    $parse = new Parse($object);
-                    if(array_key_exists('execute', $record)){
-                        return $record['execute'];
-                    }
-                    if(array_key_exists('parse', $record)){
-                        return $parse->compile('{' . $record['parse'] . '}', $storage, $object);
-                    } else {
-                        $result = $parse->compile($record['value'], $storage, $object);
-                    }
-                    if(
-                        is_string($result) &&
-                        substr($result, 0, 1) === '"' &&
-                        substr($result, -1) === '"'
-                    ){
-                        $result = substr($result, 1, -1);
-                    }
-                    return $result;
-                case Token::TYPE_QUOTE_SINGLE_STRING:
-                    return substr($record['value'], 1, -1);
-            }
-            if(array_key_exists('execute', $record)){
+        switch($record['type']){
+            case 'string':
                 return $record['execute'];
-            }
-            elseif(array_key_exists('parse', $record)){
-                $object = $this->object();
-                $storage = $this->storage();
-                $parse = new Parse($object);
-                if(array_key_exists('parse', $record)){
-                    return $parse->compile('{' . $record['parse'] . '}', $storage, $object);
-                }
-            } else {
-                return $record['value'];
-            }
+            default:
+                d($record);
+                throw new Exception('Unknown type: ' . $record['type']);
         }
-        if(!is_array($record['collection'])){
-            if(array_key_exists('execute', $record)){
-                return $record['execute'];
-            }
-            elseif(array_key_exists('parse', $record)){
-                $object = $this->object();
-                $storage = $this->storage();
-                $parse = new Parse($object);
-                if(array_key_exists('parse', $record)){
-                    return $parse->compile('{' . $record['parse'] . '}', $storage, $object);
-                }
-            } else {
-                return substr($record['value'], 1, -1);
-            }
-        }
-        foreach($record['collection'] as $nr => $item){
-            if(array_key_exists('execute', $item)){
-                $attribute .= $item['execute'];
-            }
-            elseif(array_key_exists('parse', $item)){
-                $object = $this->object();
-                $storage = $this->storage();
-                $parse = new Parse($object);
-                if(array_key_exists('parse', $record)){
-                    $attribute .= $parse->compile('{' . $item['parse'] . '}', $storage, $object);
-                }
-            } else {
-                $attribute .= $item['value'];
-            }
-        }
-        return $attribute;
+        return false;
     }
 }
