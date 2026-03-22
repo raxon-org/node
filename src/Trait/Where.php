2910a170747622fac17062452e23953fd763b1f9
Remco van der Velde <remco@universeorange.com>
2026-03-22 14:36:40 +0100

    init

--- src/Trait/Where.php
+++ src/Trait/Where.php
@@ -442,6 +442,8 @@ trait Where {
                                     'strict' => $set[2]['strict'] ?? true
                                 ]
                             ];
+                            d($list);
+                            d($filter_where);
                             $right = Filter::list($list)->where($filter_where);
                         }
                     }
