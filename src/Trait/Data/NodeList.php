ac7c5361ccad04f00e9df7eefc1f4b6d2bea9889
Remco van der Velde <remco@universeorange.com>
2026-08-08 21:37:24 +0200

    init

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -55,6 +55,7 @@ trait NodeList {
 //        d($name);
         $object = $this->object();
         $parse = false;
+        trace();
         d($options);
         if (!array_key_exists('function', $options)) {
             $options['function'] = __FUNCTION__;
