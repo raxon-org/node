e68c1829fe53c805616570785c451590bd1f15b3
Remco van der Velde <remco@universeorange.com>
2026-08-11 08:42:01 +0200

    init

--- src/Trait/Data/NodeList.php
+++ src/Trait/Data/NodeList.php
@@ -41,7 +41,7 @@ trait NodeList {
     {
         set_time_limit(30 * 60 * 60);
         if(stristr($class, 'account.role')){
-            ddd($options);
+            dd($options);
         }
         $mtime = false;
         $name = Controller::name($class);
